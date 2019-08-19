c69a0a3f2b73f0e89cf860c8f39f72246baec3e0
shaitanshamma <kurbatov_andr@hotmail.com>
2019-08-19 23:59:59 +0300

    Число в степени

--- src/main/java/lesson5/PowerOfNumber.java
+++ src/main/java/lesson5/PowerOfNumber.java
@@ -1,5 +1,29 @@
 package lesson5;
 
+import java.io.IOException;
+
 public class PowerOfNumber {
 
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(powerOfNumb(5, 3));
+    }
+
+    private static int powerOfNumb(int number, int rank) throws IOException {
+        int result = 0;
+
+        if (rank < 0) {
+            throw new IOException("Не сильная математика (: ");
+        }
+        if (rank == 0) {
+            return 1;
+        }
+        if (rank > 0) {
+            for (int i = 0; i <= rank; i++) {
+                result = number * powerOfNumb(number, rank - 1);
+
+            }
+        }
+        return result;
+    }
 }
