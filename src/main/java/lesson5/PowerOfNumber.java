d2c3183e591b49ce4511de1ee6628e5eb4934d85
shaitanshamma <kurbatov_andr@hotmail.com>
2019-08-20 19:03:08 +0300

    Число в степени версия финал

--- src/main/java/lesson5/PowerOfNumber.java
+++ src/main/java/lesson5/PowerOfNumber.java
@@ -1,22 +1,21 @@
 package lesson5;
 
-import java.io.IOException;
 
 public class PowerOfNumber {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        System.out.println(powerOfNumb(5, 3));
+        System.out.println(powerOfNumb(5, -3));
     }
 
-    private static int powerOfNumb(int number, int rank) throws IOException {
-        int result = 0;
+    private static double powerOfNumb(int number, int rank) {
+        double result = 0;
 
         if (rank < 0) {
-            throw new IOException("Не сильная математика (: ");
+            result = 1 / (powerOfNumb(number, -1 * rank));
         }
         if (rank == 0) {
-            return 1;
+            result = 1;
         }
         if (rank > 0) {
             for (int i = 0; i <= rank; i++) {
